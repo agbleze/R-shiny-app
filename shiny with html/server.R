8aa002b84353fd798805b3bf97210b4c518b4524
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-06-23 20:29:11 +0200

    Add files via upload

---
+++ shiny with html/server.R
@@ -0,0 +1,18 @@
+### load library
+library(shiny)
+
+
+### define the server logic
+function(input, output) {
+  output$textDisplay <- renderText({
+    paste0("Title: '", input$comment,
+           "'. There are ", nchar(input$comment),
+           "characters in this."
+           )
+  })
+  
+  output$plotDisplay <- renderPlot({
+    par(bg = "#ecf1ef") # set the background color
+    plot(poly(1:100, as.numeric(input$graph)), type = "l", ylab="y", xlab="x")
+  })
+}
